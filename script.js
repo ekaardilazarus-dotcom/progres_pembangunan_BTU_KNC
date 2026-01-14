0b156f5816b5e1137f7c44a4e9f59c96c87b43cf
EkaLazarus <ekaardilazarus@gmail.com>
2026-01-14 15:41:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // versi 0.334
 const USER_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx08smViAL2fT_P0ZCljaM8NGyDPZvhZiWt2EeIy1MYsjoWnSMEyXwoS6jydO-_J8OH/exec';
-const PROGRESS_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxG9fcDyHvgW8GYWtfOsHodO6yewMQssK0Ipvu-bW6ZlYgPFfPwzUq3rOWsW0ruqIHXWg/exec';
+const PROGRESS_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwfBajej9-bUiRJMgcVMT-QO8tQEycI8HWmhY8awoqizUs3nf9Sgabgb88wDa0dzZN2eg/exec';
 
 let currentRole = null;
 let selectedKavling = null;
