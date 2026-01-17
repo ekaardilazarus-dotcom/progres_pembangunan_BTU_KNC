7a74dcec605a64442df5e6a4c98c30f595fc857e
EkaLazarus <ekaardilazarus@gmail.com>
2026-01-17 12:01:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// versi 0.4
+// versi 0.45
 const USER_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx08smViAL2fT_P0ZCljaM8NGyDPZvhZiWt2EeIy1MYsjoWnSMEyXwoS6jydO-_J8OH/exec';
 const PROGRESS_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwsAzZ8bUgp-jyWN09CNQ7_qLCOn7qfzqhoXOMjNKQ3GduLH5e7ySD_qdgQSO1wXeZTtQ/exec';
 
