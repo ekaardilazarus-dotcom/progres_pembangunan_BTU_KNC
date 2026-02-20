02ee00c9d64fb805fd6839e5ec8186ac65370516
EkaLazarus <ekaardilazarus@gmail.com>
2026-02-20 15:27:09 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -11,8 +11,7 @@ const defaultDisplayNames = {
 const MAX_REVISI_PHOTOS = 6;
 window.USER_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx08smViAL2fT_P0ZCljaM8NGyDPZvhZiWt2EeIy1MYsjoWnSMEyXwoS6jydO-_J8OH/exec';
 // Base URL untuk Progress dan Utilitas
-window.BASE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzKFApQ-zm16k6GqzQOn6W9xMCKUDXNwjLyJQB7yZ2-QQki-cjrv-VMOm7TIEnyIIXYPQ/exec';
+window.BASE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzqUxW1y-OnBmwYnvHzY8tTioOyFZwrNGLvDu7TZqPDn4WRHxpbet99yQuIt36TTI7-yw/exec';
 window.PROGRESS_APPS_SCRIPT_URL = window.BASE_APPS_SCRIPT_URL;
 window.ADMIN_UTILITAS_URL = window.BASE_APPS_SCRIPT_URL;
 window.ADMIN_UTILITAS_URL_SUPERVISOR = window.BASE_APPS_SCRIPT_URL;
-
